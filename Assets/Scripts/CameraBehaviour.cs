d7fd784d89f10eaaf684ea5fd074a96e9d4a2d3f
Gabriel Aragão Ferreira da Silva <gabrielaragaofds@gmail.com>
2021-02-28 00:45:57 -0300

    smooth camera and initial store behaviour added

---
+++ Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    public float velocidadeAnimacao = 1f;
+    float x,y;
+    bool seguirPlayer;
+    
+
+    public Vector3   PosicaoInicial
+    {
+        get;set;        
+    }
+    public Vector3 PosicaoFinal
+    {
+        get;set;        
+    }
+    public Vector3 AngulacaoInicial
+    {
+        get;set;        
+    }
+
+    public Vector3 AngulacaoFinal
+    {
+        get;set;        
+    }
+    public void SetPosicaoCamera(Vector3 novaPosicaoF,Vector3 novaAngulacaoF)
+    {
+        if(seguirPlayer)
+            return;
+
+        AngulacaoInicial = transform.eulerAngles;
+        PosicaoInicial = transform.position;
+
+        AngulacaoFinal = novaAngulacaoF;
+        PosicaoFinal = novaPosicaoF;
+        x = 0;
+        StartCoroutine("MovimentarCamera");
+
+
+    }
+    IEnumerator MovimentarCamera() {
+    
+        while(x<= 1){
+
+            x += (velocidadeAnimacao * Time.deltaTime);
+            y = -x * x + 2 * x;
+            
+            Quaternion newEulerAngle = Quaternion.Euler(AngulacaoFinal);
+            transform.rotation = Quaternion.SlerpUnclamped(transform.rotation, newEulerAngle,y/10);
+            transform.localPosition = Vector3.Lerp(PosicaoInicial,PosicaoFinal, y);
+            yield return null;
+       } 
+  
+    }
+}
---
+++ Assets/Scripts/LojaBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LojaBehaviour : MonoBehaviour
+{
+    public GameObject[,] itens;
+    public int linhas,colunas;
+    public float distanciamentoX,distanciamentoY,offsetX,offsetY;
+    public GameObject PLACEHOLDER;
+    Vector3 posicaoCameraInicial,AngulacaoInicial;
+    
+    void EntrandoLoja()
+    {
+        Camera.main.GetComponent<CameraBehaviour>().SetPosicaoCamera(posicaoCameraInicial,AngulacaoInicial);
+    }
+    void Awake()
+    {
+        itens = new GameObject[linhas,colunas];
+        DeclararItens();
+    }
+    void DeclararItens()
+    {
+        for(int i = 0;i<itens.GetLength(0);i++)
+        {
+            for(int k = 0;k<itens.GetLength(1);k++)
+            {
+                itens[i,k] = Instantiate(PLACEHOLDER);
+                itens[i,k].transform.position = transform.position + new Vector3(distanciamentoX * k + offsetX,distanciamentoY * i + offsetY);
+                itens[i,k].transform.eulerAngles = new Vector3(15,0,0);
+                itens[i,k].transform.name = i.ToString() + k.ToString();
+            }    
+        }
+
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            EntrandoLoja();
+        }
+    }
+}
