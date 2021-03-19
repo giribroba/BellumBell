90afb1daee537a908929578cd07055b69edfbf53
Gabriel <gabrielaragaofds@gmail.com>
2021-03-19 19:55:02 -0300

    blend tree animation changed

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -24,14 +24,16 @@ public class PlayerBehaviour : MonoBehaviour
       Vector3 movement = Vector3.zero;
       float v = Input.GetAxis("Vertical");
       float h = Input.GetAxisRaw("Horizontal");
-      velocidadeAtual =Input.GetKey(KeyCode.LeftShift)? velocidade*2f:velocidade;
-      transform.Rotate(Vector3.up *h *velocidadeAtual/(velocidade +30));
+      velocidadeAtual =Mathf.Clamp(Input.GetKey(KeyCode.LeftShift)? velocidadeAtual  + Time.deltaTime * 80  : velocidadeAtual,velocidade ,velocidade*2);
+       velocidadeAtual -= Time.deltaTime *40;
+      transform.Rotate(Vector3.up *h *velocidadeAtual/(velocidade +10));
       transform.GetComponent<Animator>().SetFloat("Velocidade",controle.velocity.magnitude);
       movement += transform.forward * v * velocidadeAtual * Time.deltaTime;
+      movement += Physics.gravity;
       controle.Move(movement);
       //movement += transform.right * h * velocidade * Time.deltaTime;
       //movement = transform.TransformDirection(movement);
-      movement += Physics.gravity;
+     
       
     }
 }
