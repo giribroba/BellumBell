f0f88860b9f4ed438ac4b9d0e4c2a64f7429d16b
gabriel aragao <gabrielaragaofds@gmail.com>
2021-03-23 00:17:09 -0300

    android mobile joysticks,one button, platform dependent compilation added

---
+++ Assets/Scripts/ButtonBheaviour.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBheaviour : MonoBehaviour
+{
+  
+    public void Interacao()
+    {
+        GameObject.FindWithTag("Player").GetComponent<PlayerBehaviour>().BotaoInteracaoApertado();
+    }
+}
--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -1,14 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
     public CharacterController controle;
+    public CinemachineFreeLook cinemachine;
+    public Canvas JoystickCanvasMobile;
     public float velocidade;
     public float tempoSuavidade;
+    Joystick joyPlayer,joyCamera;
     float velocidadeCorrida;
     float tornarSuave;
+    float v,h;
+    void Awake()
+    {
+       #if UNITY_ANDROID 
+       //|| UNITY_EDITOR
+        Canvas joysticks = Instantiate(JoystickCanvasMobile);
+        joyPlayer = joysticks.transform.GetChild(2).GetComponent<Joystick>();
+        joyCamera = joysticks.transform.GetChild(1).GetComponent<Joystick>();
+        cinemachine.m_YAxis.m_InputAxisName ="";
+        cinemachine.m_XAxis.m_InputAxisName ="";
+        velocidade = velocidade * 1.05f;
+        #endif
+    }
     void Start()
     {
         velocidadeCorrida = velocidade;
@@ -18,16 +35,31 @@ public class PlayerBehaviour : MonoBehaviour
     {
         Movimentacao();
     }
+    public void BotaoInteracaoApertado()
+    {
+        print("pei pou");
+    }
     void Movimentacao()
     {
 
         Vector3 movimento = Vector3.zero;
-        float v = Input.GetAxis("Vertical");
-        float h = Input.GetAxisRaw("Horizontal");
 
+        #if UNITY_STANDALONE
+        v = Input.GetAxis("Vertical");
+        h = Input.GetAxisRaw("Horizontal");
         velocidadeCorrida = Mathf.Clamp(Input.GetKey(KeyCode.LeftShift)? velocidadeCorrida  + Time.deltaTime * 80  : velocidadeCorrida,velocidade ,velocidade*2);
-        velocidadeCorrida -= Time.deltaTime *40;
+        #endif
+
+        #if UNITY_ANDROID
+        // || UNITY_EDITOR
+        v = joyPlayer.Vertical;
+        cinemachine.m_YAxis.Value += joyCamera.Vertical/60;
+        cinemachine.m_XAxis.Value += joyCamera.Horizontal;
+        h = joyPlayer.Horizontal;
+        velocidadeCorrida = Mathf.Clamp(velocidadeCorrida  + Time.deltaTime * 80  ,velocidade ,velocidade*2f);
+        #endif
         
+        velocidadeCorrida -= Time.deltaTime *40;
         //caso ele fique parado em uma parede apertando w + shift, ele ja não saia correndo sem antes acelerar
         velocidadeCorrida = controle.velocity.magnitude <= 33 ? velocidade : velocidadeCorrida;
 
